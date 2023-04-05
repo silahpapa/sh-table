6f56925e785508fe341c0b252f1c96bac55a58d4
silah <silakosy@gmail.com>
2023-04-05 18:58:05 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import post from './components/Post.vue'
+import Post from "./components/Post.vue";
+</script>
+<template>
+  <post  />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ShTable from "./ShTable.vue";
+
+</script>
+<template>
+  <div class="container">
+    <div class="row mt-5">
+      <sh-table msg="test"></sh-table>
+    </div>
+  </div>
+</template>
